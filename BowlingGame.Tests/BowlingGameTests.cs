1814a1fe15723dfde80e7fce409c08b5b757a7eb
bartek_zaczek <32414538+badziu17@users.noreply.github.com>
2018-03-19 20:32:53 +0100

    Add files via upload

---
+++ BowlingGame.Tests/BowlingGameTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        private Game _game;
+        [SetUp]
+        public void Init()
+        {
+            _game = new Game();
+        }
+        [Test]
+        public void Should_GutterGame_ScoresZero()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _game.Roll(0);
+            }
+
+            Assert.That(_game.TotalScores(), Is.EqualTo(0));
+
+        }
+        [Test]
+        public void Should_AllOnes_ScoreTwenty()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _game.Roll(1);
+            }
+            Assert.That(_game.TotalScores(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Should_SpareThan3ThenAllMisses_Scores16()
+        {
+            //spare in the first time
+
+            _game.Roll(6);
+            _game.Roll(4);
+            // that 3 pins
+            _game.Roll(3);
+            
+            //then all gutters missed 
+            for (int i = 0; i < 17; i++)
+            {
+                _game.Roll(0);
+            }
+            Assert.That(_game.TotalScores(), Is.EqualTo(16));
+        }
+
+        [Test]
+        public void Should_StrikeThen3pinsThen4pinsThenAllMisses_Scores24()
+        {
+            _game.Roll(10);
+            _game.Roll(3);
+            // that 3 pins
+            _game.Roll(4);
+
+            //then all gutters missed 
+            for (int i = 0; i < 16; i++)
+            {
+                _game.Roll(0);
+            }
+            Assert.That(_game.TotalScores(), Is.EqualTo(24));
+        }
+        [Test]
+        public void Should_PerfectGame_Scores300()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                _game.Roll(10);
+            }
+            Assert.That(_game.TotalScores(), Is.EqualTo(300));
+        }
+    }
+}
+
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        List<Roll> _rolls;
+        public Game()
+        {
+            _rolls = new List<Roll>();
+        }
+
+        public void Roll(int pins)
+        {
+            _rolls.Add(new Roll()
+            {
+                KnockedPinsCount = pins
+            });
+        }
+
+        //public int TotalScores()
+        //{
+        //    int result = 0;
+
+        //    for (int i = 0; i < _rolls.Count; i++)
+        //    {
+        //        //_rolls[i].KnockedPinsCount;
+        //        if (i + 1 < _rolls.Count && IsSpare(i))
+        //        {
+        //            result += 10 + _rolls[i + 2].KnockedPinsCount;
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            result += _rolls[i].KnockedPinsCount;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        public int TotalScores()
+        {
+            int result = 0;
+            int rollIndicator = 0;
+            int frameIndicator = 0; 
+
+            while (frameIndicator < 10)
+            {
+                if (IsStrike(rollIndicator))
+                {
+                    result += 10 + _rolls[rollIndicator + 1].KnockedPinsCount + _rolls[rollIndicator + 2].KnockedPinsCount;
+                    rollIndicator += 1;
+                }
+                else if (IsSpare(rollIndicator))
+                {
+                    result += 10 + _rolls[rollIndicator + 2].KnockedPinsCount;
+                    rollIndicator += 2;
+                }
+                else
+                {
+                    result += _rolls[rollIndicator].KnockedPinsCount + _rolls[rollIndicator + 1].KnockedPinsCount;
+                    rollIndicator += 2;
+                }
+                frameIndicator++;
+            }
+
+            return result;
+        }
+
+        private bool IsStrike(int rollIndicator)
+        {
+            return _rolls[rollIndicator].KnockedPinsCount == 10;
+        }
+
+        private bool IsSpare(int i)
+        {
+            return _rolls[i].KnockedPinsCount + _rolls[i + 1].KnockedPinsCount == 10;
+        }
+    }
+}
